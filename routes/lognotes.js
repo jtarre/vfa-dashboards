2dfeeb7b49c220f552d85f758102807ecff60af8
Jason Tarre <jason.tarre@gmail.com>
2015-09-12 17:48:10 -0400

    Successfully added a 'completed task' into salesforce from web app dashboard

---
+++ routes/lognotes.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var router  = express.Router();
+var jsforce = require('jsforce');
+
+
+router.post("/", function(req, res, next) {
+	// get data from client call
+	// send notes to salesforce
+	console.log('received post request');
+
+	
+	var subject      = req.param('subject');
+	var description  = req.param('description');
+
+	console.log('subject + description: ' + subject + "\n\n" + description);
+
+	var conn = new jsforce.Connection({
+		loginUrl : 'https://login.salesforce.com/',
+	    clientSecret: '4767192206007523209', 
+	    clientId: '3MVG9rFJvQRVOvk6KGm7WX.DOBEBOr701sDMIfbMTc24Y9Dzy2lVHwadn.FsVxVXXWhL5s7Jje0tS063s_gQV',
+	    redirectUri: 'http://localhost:3000/oauth/_callback',
+	    instanceUrl: 'https://na14.salesforce.com'
+	}); 
+	conn.login("jason@ventureforamerica.org", "5588Boobooboo!", function(err, userInfo) {
+		if ( err ) { return console.error(err); }
+		console.log("Authenticated!");
+	});
+	
+	conn.sobject("Task").create({
+		Subject      : subject,
+		Description  : description,
+		OwnerId      : "005d0000001OzTa",
+		WhoId        : "003d000000mHoUy",
+		Status       : "Completed",
+		Priority     : "Normal",
+		ActivityDate : jsforce.Date.toDateTimeLiteral(new Date(2015, 8, 5))
+ 
+	}, function(err, ret) {
+		if ( err ) { return console.error(err); }
+		console.log("Id!");
+		//res.send("Notes logged!");
+	})
+	
+
+})
+
+module.exports = router;
