f64102f138af719311596d71b28151a55fdd98f0
Jason Tarre <jason.tarre@gmail.com>
2015-09-12 21:43:38 -0400

    working on surveygizmo data

--- routes/users.js
+++ routes/users.js
@@ -88,44 +88,133 @@ router.get('/', function(req, res, next) {
 			.limit(5)
 			.execute( function(err, surveys) {
 				if (err) { return console.error(err); }
-				
-				for (var i = 0; i < surveys.length; ++i) {
-					// COMPANY PARTNER SURVEY // 
-						// QUANT // 
-						var overall         = surveys[i].CPS_Overall__c;
-						var problemSolving  = surveys[i].CPS_Problem_Solving__c;
-						var professionalism = surveys[i].CPS_Professionalism__c;
-						var hardSkills      = surveys[i].CPS_Hard_Skills__c;
-						var communication   = surveys[i].CPS_Communication__c;
-						var teamFit         = surveys[i].CPS_Team_Fit__c;
-
-						// LONG FORM // 
-						var improvement     = surveys[i].CPS_Improvement__c;
-						var strength        = surveys[i].CPS_Strengths__c;
-
-					// SELF-EVALUATION SURVEY // 
-						// QUANT //
-						var performance     = surveys[i].FSE_Job_Performance__c;
-
-						// LONG FORM //
-						var workStrengh     = surveys[i].FSE_Did_Well_at_Work__c;
-						var workImprove     = surveys[i].FSE_Improve_at_Work__c;
-
-				}
-				
 				console.log(surveys);
-				res.render('users', 
-				{
-					test           : fellowId,
-					contactInfo    : contactInfo,
-					companyInfo    : companyInfo,
-					supervisorInfo : supervisorInfo
-				});			
+				console.log("We returned surveys!!!!");
+				console.log("surveys.length: " + surveys.length);
+
+				for (var i = 0; i < surveys.length; ++i) {
+					console.log("we've gone through: " + i + " surveys");
+					var companyPartnerCount = 0;
+					var companyPartnerRecordId = "012d0000000SwwyAAC";
+
+					var selfEvalCount       = 0;
+					var selfEvalRecordId    = "012d0000000SxCNAA0";
+
+					console.log("on pass " + i + " recordtypeid = " + surveys[i].RecordTypeId);
+					console.log("for the record, company record id = " + companyPartnerRecordId);
+					console.log("for the record, fellow record id = " + selfEvalRecordId);
+					console.log("on pass " + i + " companyPartnerCount = " + companyPartnerCount);
+					console.log("on pass " + i + " selfEvalCount = " + selfEvalCount);	
+					if (companyPartnerCount == 0) {
+						console.log("Made it passed first conditional!");
+						if (surveys[i].RecordTypeId.toString() == companyPartnerRecordId) {
+							console.log("made it passed second!");
+
+						companyPartnerCount = companyPartnerCount + 1;
+						// COMPANY PARTNER SURVEY // 
+							var companyPartnerName= surveys[i].Name;
+							var companyPartnerCreated= surveys[i].CreatedDate;
+							
+							// QUANT // 
+							var overall         = surveys[i].CPS_Overall__c;
+							var problemSolving  = surveys[i].CPS_Problem_Solving__c;
+							var professionalism = surveys[i].CPS_Professionalism__c;
+							var hardSkills      = surveys[i].CPS_Hard_Skills__c;
+							var communication   = surveys[i].CPS_Communication__c;
+							var teamFit         = surveys[i].CPS_Team_Fit__c;
+
+							// LONG FORM // 
+							var improvement     = surveys[i].CPS_Improvement__c;
+							var strength        = surveys[i].CPS_Strengths__c;
+						}
+					}
+
+					if (selfEvalCount == 0 && surveys[i].RecordTypeId == selfEvalRecordId) {
+						selfEvalCount = selfEvalCount + 1;
+						// SELF-EVALUATION SURVEY // 
+							var selfEvalName    = surveys[i].Name;
+							var selfEvalCreatedDate= surveys[i].CreatedDate;
+							// QUANT JOB //
+							var performance     = surveys[i].FSE_Job_Performance__c;
+							var jobHappiness    = surveys[i].FSE_Job_Happiness__c;
+							var jobChallenge    = surveys[i].FSE_Job_Challenge__c;
+
+							var salary         = surveys[i].FSE_Salary__c;
+
+							// LONG FORM JOB //
+							var workStrengh     = surveys[i].FSE_Did_Well_at_Work__c;
+							var workImprove     = surveys[i].FSE_Improve_at_Work__c;
+							var workChallenge   = surveys[i].FSE_Challenge__c;
+
+							// QUANT LIFE // 
+							var extracurricular = surveys[i].FSE_Extracurricular__c;
+							var support         = surveys[i].FSE_Support__c;
+							var socialLife      = surveys[i].FSE_Social_Life__c;
+
+							var overall         = surveys[i].FSE_Overall__c;
+					}
+					
+					if (selfEvalCount == 1 && companyPartnerCount == 1) {
+						// DATA OBJECTS: PREP FOR WEBSITE // 
+						companyInfo.salary  = salary;
+						companyInfo.equity 	= equity; 
+						
+						var companyPartnerQuant = 
+						{
+							overall         : "Overall: " + overall,
+							teamFit         : "Team Fit / Hustle: " + teamFit,
+							problemSolving  : "Problem Solving: " + problemSolving,
+							hardSkills      : "Hard Skills: " + hardSkills,
+							communication   : "Communication: " + communication,
+						};
+
+						var companyPartnerLongForm = 
+						{
+							strength        : "Strengths:\n" + strength,
+							improvement     : "Areas for Improvement:\n" + improvement
+
+						};
+
+						var selfEvaluationJobQuant = 
+						{
+							overall         : "Overall Satisfaction: " + overall,
+							performance     : "Job Performance: " + performance,
+							jobHappiness    : "Job Happiness: " + jobHappiness,
+							jobChallenge    : "Job Challenge: " + jobChallenge
+						};
+
+						var selfEvaluationLifeQuant = 
+						{
+							support         : "Support: " + support,
+							socialLife      : "Social Life: " + socialLife,
+							extracurricular : "Extracurriculars: " + extracurricular
+
+						}
+
+						var selfEvaluationJobLongForm = 
+						{
+							workStrengh     : "Work Strengths:\n" + workStrengh,
+							workImprove     : "Areas to Improve at Work:\n" + workImprove,
+							workChallenge   : "Areas of Challenge:\n" + workChallenge
+						}
+						res.render('users', 
+						{
+							test                      : fellowId,
+							contactInfo               : contactInfo,
+							companyInfo               : companyInfo,
+							supervisorInfo            : supervisorInfo,
+							companyPartnerQuant       : companyPartnerQuant,
+							companyPartnerLongForm    : companyPartnerLongForm,
+							selfEvaluationLifeQuant   : selfEvaluationLifeQuant,
+							selfEvaluationJobQuant    : selfEvaluationJobQuant,
+							selfEvaluationJobLongForm : selfEvaluationJobLongForm, 
+						});	
+					}
+				}	
+							
 		});
 
 	});
-
-	// here is where i send the data
 
 });
 
