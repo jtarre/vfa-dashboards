8f3a843dcfa8bdb7342f658d1516443275b33315
Jason Tarre <jason.tarre@gmail.com>
2015-09-12 20:12:11 -0400

    Successfully displayed contact data on web app dashboard :) :) :)

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,92 @@
-var express = require('express');
-var router = express.Router();
+var express   = require('express');
+var router    = express.Router();
+var jsforce   = require('jsforce');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  console.log("get!");
-  var id = req.param('id');
-  console.log("id: " + id);
-  res.render('users', 
-  	{
-  		test : id
-  	});
-  //res.send('respond with a resource');
+	console.log("get!");
+	var fellowId = req.param('id');
+	console.log("id: " + fellowId);
+	// todo: get the data
+
+	var conn = new jsforce.Connection({
+		loginUrl : 'https://login.salesforce.com/',
+		clientSecret: '4767192206007523209', 
+		clientId: '3MVG9rFJvQRVOvk6KGm7WX.DOBEBOr701sDMIfbMTc24Y9Dzy2lVHwadn.FsVxVXXWhL5s7Jje0tS063s_gQV',
+		redirectUri: 'http://localhost:3000/oauth/_callback',
+		instanceUrl: 'https://na14.salesforce.com'
+	}); 
+	
+	// login to salesforce. after this, can run all functions
+	conn.login("jason@ventureforamerica.org", "5588Boobooboo!", function(err, userInfo) {
+		if ( err ) { return console.error(err); }
+		console.log("Authenticated!");
+	});
+
+	// get contact info
+	conn.sobject("Contact").retrieve(fellowId, function(err, contact) {
+		if (err) { return console.error(err); }
+		console.log("Fellow Contact Data\n\n");
+		for (data in contact) {
+			console.log("key: " + data + " value: " + contact[data]);
+		};
+
+		// CONTACT INFO // 
+		var email           = contact.Email;
+		var phone           = contact.Phone;
+		var description     = contact.Description;
+
+		var birthdate       = contact.Birthdate;
+		// COMPANY INFORMATION //
+		var company         = contact.Account_Name_for_SurveyGizmo__c;
+		var companyWebsite  = contact.Account_Website__c;
+		var companyCity     = contact.Account_s_VFA_City__c;
+
+		var jobTitle        = contact.Title;
+
+		// SUPERVISOR INFO //
+		var supervisorFirst = contact.Supervisor_First_Name__c;
+		var supervisorLast  = contact.Supervisor_Last_Name__c; 
+		var supervisorEmail = contact.Supervisor_Email__c;
+		
+		console.log("Company Name: " + company);
+		console.log("email: " + email);
+		
+		
+		var contactInfo = 
+		{
+			email        : "Email: " + email,
+			phone        : "Phone: " + phone,
+			birthday	 : "Birthday: " + birthdate,
+			description  : "Bio: " + description
+
+		};
+
+		var companyInfo = 
+		{
+			company      : "Company: " + company,
+			website      : "Website: " + companyWebsite,
+			city         : "City: "    + companyCity,
+			title        : "Job Title: " + jobTitle
+		};
+
+		var supervisorInfo = 
+		{
+			name         : "Supervisor:  " + supervisorFirst + " " + supervisorLast,
+			email        : "Email: " + supervisorEmail
+		};
+
+		res.render('users', 
+		{
+			test           : fellowId,
+			contactInfo    : contactInfo,
+			companyInfo    : companyInfo,
+			supervisorInfo : supervisorInfo
+		});
+	});
+
+	// here is where i send the data
+
 });
 
 module.exports = router;
