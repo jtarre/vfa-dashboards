5e8ad5a2c03c2ec912ea28840d7cbe145729fd2d
Jason Tarre <jason.tarre@gmail.com>
2015-09-20 18:32:07 -0400

    Successfully added related to dropdown for associated cases to fellow dashboard.

--- routes/cases.js
+++ routes/cases.js
@@ -25,7 +25,7 @@ router.get('/', function(req, res, next) {
 		}
 
 	// TODO: RETRIEVE CASE
-
+	
 	conn.sobject("Case")
 		.find(
 		{
@@ -40,56 +40,28 @@ router.get('/', function(req, res, next) {
 				// view back to the browser that says try again...or something 
 				return console.error(err); 
 			}
-			// TODO: GRAB DATA
-			console.log("////////////// CASES /////////////");
-			//console.log(cases);
-			
-			//var caseList = {};
-			var fellowCase = {};
-			var caseList = {};
-			console.log("//////////// MAKING CASES ////////////");
-			console.log("cases length: " + cases.length);
+			var caseList = 
+			{
+				"" : ""
+			};
 			for ( var i = 0; i < cases.length; ++i ) {
 				// GET CASE URL, DESCRIPTION, AND CREATED DATE // 
-				//console.log("I = " + i);
-				//console.log("cases length inside for loop: " + cases.length);
+				// in this situation, all i want to do is get the subject and the id
 				var id          = cases[i].Id;
-				var createdDate = cases[i].CreatedDate;
-				var url         = process.env.INSTANCE_URL + id;
-				var description = cases[i].Description;
 				var subject     = cases[i].Subject;
 
-				console.log("///////// ADDING CASE /////////");
-				console.log(subject);
-				
-				fellowCase = {
-						createdDate : "CreatedDate: " + createdDate,
-						url         : "URL: " + url,
-						description : "Description: " + description 
-					};
 				(function () {
-					caseList[subject] = fellowCase;
+					caseList[subject] = id;
 				})();
 			}
-			/*
-			console.log("\n\nCases: ");
 			
-			for ( row in caseList ) {
-				console.log("Row");
-				console.log("object id: " + row);
-				for (object in caseList[row]) {
-					console.log("object props: " + object + " object value: " + caseList[row][object]);
-				}
-			}
-			*/
-			// TODO: RENDER WEBSITE
-			/*
 			res.render("cases",
-			{
-				caseList : caseList
-			});
-			*/
+				{
+					caseList : caseList
+				});
+			
 		});
+		
 
 	});
 });
