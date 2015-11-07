7182d936436fe70899fc8019cb3d6a4360f6c991
Jason Tarre <jason.tarre@gmail.com>
2015-11-07 16:33:23 -0500

    playing with dotenv file

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router  = express.Router();
 var jsforce = require('jsforce');
 var _       = require('underscore');
-require('dotenv').load();
 
 /* GET home page. */
 // no seriously get the home page
