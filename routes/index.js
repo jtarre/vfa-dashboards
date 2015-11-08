f186d74d545cff8e83a058bd7d5bbbc50f27f9eb
Jason Tarre <jason.tarre@gmail.com>
2015-11-08 16:26:48 -0500

    getting to the bottom of session problems

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,11 @@ var _       = require('underscore');
 
 module.exports = function(app, passport) {
     app.get("/", function(req, res) {
+    	console.log("/// user request home page ///");
+    	_.each(req.user, function(key, value, list) {
+    		console.log(key + ": " + value);
+    	});
+    	console.log("\n");
         res.render("index", {user: req.user});
     })
 }
