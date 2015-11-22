3cdf4b04e133cd1b39122848edbfd9e7f86c93e3
Jason Tarre <jason.tarre@gmail.com>
2015-11-21 22:04:32 -0500

    cleaning up companies template.

--- public/javascripts/partial/data/data.js
+++ public/javascripts/partial/data/data.js
@@ -1,4 +1,4 @@
-vfaDashboard.controller("DataCtrl", function($scope, $http) {
+vfaDashboard.controller("dataCtrl", function($scope, $http) {
 
 		$scope.options = {
 			chart: {
@@ -40,31 +40,4 @@ vfaDashboard.controller("DataCtrl", function($scope, $http) {
 		        { "label" : "H" , "value" : -5.1387322875705 }
 		        ]
 		}];
-	})
-
-/* 
-chart: {
-        type: 'discreteBarChart',
-        height: 450,
-        margin : {
-            top: 20,
-            right: 20,
-            bottom: 60,
-            left: 55
-        },
-        x: function(d){ return d.label; },
-        y: function(d){ return d.value; },
-        showValues: true,
-        valueFormat: function(d){
-            return d3.format(',.4f')(d);
-        },
-        transitionDuration: 500,
-        xAxis: {
-            axisLabel: 'X Axis'
-        },
-        yAxis: {
-            axisLabel: 'Y Axis',
-            axisLabelDistance: 30
-        }
-    }
- */
+	});
