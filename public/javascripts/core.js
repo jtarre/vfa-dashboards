8cadd05ac1705e6d86b35f292e1bb42f38d7baa1
Jason Tarre <jason.tarre@gmail.com>
2015-11-21 22:31:18 -0500

    sample data chart working. for some reason, couldn't create data controller in separate file. needed to write in core.js. next up, grouping accounts into createdBy months to prepare for charting.

--- public/javascripts/core.js
+++ public/javascripts/core.js
@@ -5,6 +5,50 @@ underscore.factory('_', ['$window', function($window) {
 
 var vfaDashboard = angular.module("vfaDashboard", ["ui.router", "underscore", "nvd3"]);
 
+vfaDashboard.controller("dataCtrl", function($scope, api) {
+    
+    $scope.options = {
+        chart: {
+            type: 'discreteBarChart',
+            height: 450,
+            margin : {
+                top: 20,
+                right: 20,
+                bottom: 60,
+                left: 55
+            },
+            x: function(d){ return d.label; },
+            y: function(d){ return d.value; },
+            showValues: true,
+            valueFormat: function(d){
+                return d3.format(',.4f')(d);
+            },
+            transitionDuration: 500,
+            xAxis: {
+                axisLabel: 'X Axis'
+            },
+            yAxis: {
+                axisLabel: 'Y Axis',
+                axisLabelDistance: 30
+            }
+        }
+    };
+
+    $scope.data = [{
+        key: "Cumulative Return",
+        values: [
+            { "label" : "A" , "value" : -29.765957771107 },
+            { "label" : "B" , "value" : 0 },
+            { "label" : "C" , "value" : 32.807804682612 },
+            { "label" : "D" , "value" : 196.45946739256 },
+            { "label" : "E" , "value" : 0.19434030906893 },
+            { "label" : "F" , "value" : -98.079782601442 },
+            { "label" : "G" , "value" : -13.925743130903 },
+            { "label" : "H" , "value" : -5.1387322875705 }
+            ]
+    }];
+});
+
 
 vfaDashboard.config(function($stateProvider, $urlRouterProvider) {
 
@@ -83,20 +127,3 @@ vfaDashboard.run(function($rootScope) {
     };
 
 });
-
-
-/*
-    what now?
-    login up & running
-    now two areas
-    bar charts
-    and google maps
-    if I can get one of each going
-    on the data front,
-    I already have some amount of CoPa data
-    I think I'll need to clean it. 
-    I'll need each account by their created date
-    and I'll need to somehow bucket them
-    let's read documentation.
- */
-
