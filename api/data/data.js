5793d871152a48cecc46afeb46f3228ef29c6055
Jason Tarre <jason.tarre@gmail.com>
2015-11-21 23:54:51 -0500

    progress on company count. hard-coded and manual in version 1.

--- api/data/data.js
+++ api/data/data.js
@@ -19,80 +19,55 @@ module.exports = function(app) {
 				.execute( function(err, accounts) {
 					if (err) { return console.error(err); }
 					console.log(accounts.length);
+					var date = new Date();
+					var month = date.getMonth();
+					var year  = date.getFullYear();
+					console.log("month", month);
+					console.log("year", year);
+					year--;
+					month++;
 
-					_.countBy(accounts, function(account) {
-						/*
-							a: create empty object to hold month/years
-							b: add empty object ...
-							1: get account month and year
-							2: create unique string out of month and year
-							3: check for existence 
-						 */
+					var startYear = year;
+					var startMonth= month;
 
-						 /*
-						 	how to take into account counts of 0?
-						 	if in September 14, you had 0 accounts
-						 	I'd have to add in before to make it work
-						 	so how can I first create months for as many as I need?
-						 	1. create new date
-						 	2. get today's month
-						 	3. get today's year
-						 	down the road: get string names for each month
-						 	4. start by pulling last 12 months of account creation 
-						 	4a. 12 months is easy.
-						 	5. get today's year - 1
-						 	6. create bucket of month year combos
-						 	6a. for each via underscore
-						 	6b. get month / year create string out of it.
-						 	6c. add to bucket object. set value initially to 0. 
+					var monthsUntilNewYear = 12 - month;
 
-						 	question: how to for each on a wrapping number set? 
+					var calendar = {};
 
-						 	hmmm...what's my list? what will the end result look like
+					for (var i = 0; i < 13; ++i) { // 13 months gets previous year + current month
+						if (month == 0) {
+							year++;
+						}
+						calendar[month + " " + year] = 0;
+						
+						month = (month + 1) % 12;
+						monthsUntilNewYear = (monthsUntilNewYear - 1) % 12;
+					}
+					console.log("calendar\n", calendar);
+					// list made, now add relevant accounts
+					var accountCreatedDate;
+					var accountCreatedMonth;
+					var accountCreatedYear;
+					_.each(accounts, function(element, index, list) {
+							console.log("new element: element date - ", element.CreatedDate);
 							
-							date = new Date();
-							month = date.getMonth();
-							year  = date.getYear();
+							accountCreatedDate =  new Date (element.CreatedDate);
+							accountCreatedMonth=  accountCreatedDate.getMonth();
+							accountCreatedYear =  accountCreatedDate.getFullYear();
+							
+							// console.log("account month", accountCreatedDate, accountCreatedMonth, accountCreatedYear);
 
-							firstYear = year - 1;
-							var monthsUntilNewYear = 12 - month;
-
-							for (var i = 0; i < 12; ++i) {
-								if (month == 0) {
-									year++; // what about january? 
-								}
-								calendar[month + " " + year] = 0;
-								month = (month + 1) % 12;
-								monthsUntilNewYear = (monthsUntilNewYear - 1) % 12;
+							var monthYearInCalendar = accountCreatedMonth + " " + accountCreatedYear;
+							if(monthYearInCalendar in calendar) {
+								console.log("	created date in can");
+								calendar[accountCreatedMonth + " " + accountCreatedYear] =
+								1 + calendar[accountCreatedMonth + " " + accountCreatedYear];
+							}  else {
+								console.log("	null");
 							}
-							month = from a list
-							year  = from a list
-						 	calendar[month + " " + year] = 0;
-
-						 	rats soooo let's say you've got january 2014. subtract 1. jan 2013
-						 	if jan == 0 increment 1. back where we started. 
-						 	and depending on where you are in the year. 
-						 	i could make it more complicated. count down (or up) until the new year
-						 	var monthsUntilNewYear = 11 - month. 
-						 	this is not a generalizable solution. dates surprisingly difficult to work with. 
-
-
-
-						  */
-
-						/*
-						todo: organize by months
-						at least two parts two this
-						part 1: take createddate and get month and year
-						part 2: add account to appropriate bucket in object
-						part 3: give each bucket an appropriate label
-							
-						 */
-					})
+					});
+					console.log("calendar after each loop", calendar);
 				});
 		});
 	});
 }
-/*
-	let's make some magic happen
- */
