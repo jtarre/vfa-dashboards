10fd3da0ec3ed5d1d4ae5eea364e6693f6d02d2d
Jason Tarre <jtarre@users.noreply.github.com>
2015-11-27 21:15:52 -0500

    sketching out cities page on server side. will...
    need to npm install when i get my comp back.

--- api/cities/cities.js
+++ api/cities/cities.js
@@ -1,2 +1,20 @@
 module.exports = function(app) {
+	app.get("/api/cities", function(req, res) {
+		var MapboxClient = requre('mapbox');
+		var mapbox       = MapboxClient('ACCESS TOKEN');
+		var jsforce;
+		var _;
+		
+		// new conn
+		
+		// login
+		
+		// get accounts in vfa city cleveland
+		
+		// _.each(accounts)
+		// account.mailing address if
+		//mapbox.geocodeForward(mailingAddress, fn(res) {)
+		//res.status(200).json(geocodeMailingAddresses)
+		// leaflet client side
+	})
 }
