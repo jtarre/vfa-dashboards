19c382fd546bfa7d5cbb0d6748570ca8c7ba46b6
Jason Tarre <jtarre@users.noreply.github.com>
2015-11-29 11:35:09 -0500

    started work on client-side cities.js file

--- public/javascripts/partial/cities/cities.js
+++ public/javascripts/partial/cities/cities.js
@@ -1,1 +1,17 @@
-
+vfaDashboard.controller("cityMapCtrl", function($scope, $http, 'leafletData') {
+	
+	$scope.markers;
+	$scope.cityCenter;
+	
+	$scope.getCityData = function(cityAndState) {
+		api.cities.getCityData(cityAndState).then(function( response ) {
+			$scope.cityData = response.markers;
+			$scope.markers = cityDataToCityMarkers($scope.cityData);
+			$scope.cityCenter = response.cityCenter;
+		})
+	}
+	
+	$scope.cityDataToCityMarkers = function(cityData) {
+		
+	}
+})
