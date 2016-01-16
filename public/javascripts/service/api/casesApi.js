34b5532bdaa87a75eecefbb65272367b72fc3589
Holly Graham <HollyG@holly-grahams-macbook-pro.local>
2016-01-16 17:17:37 -0500

    added auth service to be used in the future.

--- public/javascripts/service/api/casesApi.js
+++ public/javascripts/service/api/casesApi.js
@@ -1,23 +1,36 @@
 angular.module('vfaDashboard').factory('casesApi', function($http) {
 	return {
-		create: function create(subject, description, user, fellow) {
+		create: function create(subject, description, user, fellowId, type) {
 			var newCase = {
 				Subject: subject,
 				Description: description,
+				Origin: "Email",
+				Status: "In Progress",
+				Type: type,
 				OwnerId: user.Id,
-				WhoId: fellow.profile.Id
+				ContactId: fellowId
 			}; 
 			return $http.post("/api/cases", newCase)
 				.then( function (response) {
+					console.log("new case response: ", response);
+
 					return response.data;
 				})
 		},
 
 		getForFellow: function getForFellow(id) {
-			return $http.get("/api/cases/fellows/:id")
+			return $http.get("/api/cases/fellows/" + id)
 				.then( function (response) {
 					return response.data;
 				})
+		},
+
+		getTypes: function getTypes() {
+			return $http.get("/api/cases/types")
+				.then(function(response) {
+					console.log("get case types: ", response);
+					return response.data;
+				});
 		}
 	}
 })
