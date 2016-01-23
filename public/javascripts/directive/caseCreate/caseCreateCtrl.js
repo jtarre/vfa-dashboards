aa01706cebe15861d28c7f57869d12e1675e1eb4
Jason Tarre <jason.tarre@gmail.com>
2016-01-22 23:43:53 -0500

    work continues.

--- public/javascripts/directive/caseCreate/caseCreateCtrl.js
+++ public/javascripts/directive/caseCreate/caseCreateCtrl.js
@@ -3,17 +3,51 @@ angular.module('vfaDashboard').controller('CaseCtrl', function($scope, _, api, c
 		$scope.users = data;
 	});
 
-	$scope.createCase = function createCase(newCase) {
-		newCase = _.map(newCase, function(newCaseInput) {
-						if(_.isNull(newCaseInput)) {
-							newCaseInput = "";
-						}
-					});
-		// let's think about this in the lodash way. 
-		// i want to check if each input is null
-		// and make them "" for the ones that are
-		// and do nothing for the ones that aren't. 
+	$scope.contacts = [];
+	$scope.contactSearch = "";
+	$scope.isContactSearchInProgress = false;
+	$scope.noContactsFound = false;
+	$scope.noContactsFoundMessage = "No Contacts Found. Search Again";
+	$scope.$watch('contactSearch', function(newValue) {
+		if(newValue.length > 5) { 
+			if($scope.isContactSearchInProgress) {
+				$scope.searchContacts(newValue);	
+			}	
+		}
 
-		casesApi.create(newCase.Subject, newCase.Description, newCase.user, newCase.contact, newCase.type);
+		if(newValue.length <= 5) { // reset if person deletes name
+			if($scope.isContactSearchInProgress) {
+				$scope.isContactSearchInProgress = false;
+			}
+		}
+	});
+
+	$scope.searchContacts = function searchContacts(search) {
+		$scope.isContactSearchInProgress = true; // this really should be applied to the method, not tied on client-side.
+		$scope.noContactsFound = false;
+		$scope.contacts = [];
+		api.contacts.getBySearch(search).then(function(data) {
+			console.log("note logger ctrl response: ", data);
+			$scope.isContactSearchInProgress = false;
+			if(data.length) {
+				$scope.contacts = data;	
+			} else {
+				$scope.noContactsFound = true;
+			}
+			
+		});
+	}
+
+	$scope.createCase = function createCase(newCase) {
+		console.log("new case before map", newCase);
+
+		newCase = _.map(newCase, function(newCaseInput) {
+			if(_.isNull(newCaseInput)) {
+				newCaseInput = "";
+			}
+		});
+		console.log("new case after map", newCase);
+
+		// casesApi.create(newCase.Subject, newCase.Description, newCase.user, newCase.contact, newCase.type);
 	}
 })
