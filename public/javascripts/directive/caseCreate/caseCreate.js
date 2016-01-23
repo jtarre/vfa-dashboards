165cc2021b95bb21128610bb7038c685400726d4
Jason Tarre <jason.tarre@gmail.com>
2016-01-22 22:59:11 -0500

    adding html to home page + caseCreate tab.

---
+++ public/javascripts/directive/caseCreate/caseCreate.js
@@ -0,0 +1,13 @@
+angular.module('vfaDashboard').directive('caseCreate', function(_) {
+	return {
+		restrict: "E",
+		templateUrl: "javascripts/directive/caseCreate/caseCreate.html",
+		controllerUrl: "javascripts/directive/caseCreate/caseCreateCtrl.js",
+		link: function (scope, element, attrs) {
+			console.log("elements: ", element);
+			console.log("attrs: ", attrs);
+			console.log("scope: ", scope);
+		}	
+	}
+	
+})
--- public/javascripts/directive/caseCreate/caseCreateCtrl.js
+++ public/javascripts/directive/caseCreate/caseCreateCtrl.js
@@ -1,9 +1,9 @@
-angular.module('vfaDashboard').controller('CaseCtrl', function($scope, api, casesApi) {
+angular.module('vfaDashboard').controller('CaseCtrl', function($scope, _, api, casesApi) {
 	api.users.getAll().then(function(data) {
 		$scope.users = data;
 	});
 
-	$scope.createCase = function createCase() {
-		caseApi.create(subject);
+	$scope.createCase = function createCase(newCase) {
+		casesApi.create(subject);
 	}
 })
