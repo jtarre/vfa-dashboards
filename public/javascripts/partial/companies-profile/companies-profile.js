84e52081330c8e193d70b4e3192ffc8fccdf7ed4
Jason Tarre <jason.tarre@gmail.com>
2016-01-28 23:25:51 -0500

    refactoring compay page to be a search.

--- public/javascripts/partial/companies-profile/companies-profile.js
+++ public/javascripts/partial/companies-profile/companies-profile.js
@@ -4,11 +4,6 @@ vfaDashboard.controller("companyCtrl", function($scope, $stateParams, $q, accoun
 	$scope.relatedTo = [];
 	$scope.accountId = $stateParams.companyId;
 
-	// company info is an array of objects 
-	// the array is for use organizing the html
-	// the type is to specify what type of html input type it corresponds to. 
-	// there's got to be a better way to do it.
-
 	$scope.companyInfo2 = [
 		"Id",
 		"CoPa_Association__c",
@@ -28,56 +23,74 @@ vfaDashboard.controller("companyCtrl", function($scope, $stateParams, $q, accoun
 		"Twitter_Handle__c"
 	];
 
-	$scope.companyInfo = [];
-	api.companies.getFields().then(function(data) {
-		 var allFields = _.map(data.fields, function(field) { // trim the meta fields to a subset of values.
-		 	return {name: field.name, type: field.type, label: field.label, picklistValues: field.picklistValues}
-		 });
+	var getCompanyFields = function getCompanyFields() {
+		var deferred = $q.defer();
+		api.companies.getFields().then(function(data) {
+			 var allFields = _.map(data.fields, function(field) { // trim the meta fields to a subset of values.
+			 	return {name: field.name, type: field.type, label: field.label, picklistValues: field.picklistValues}
+			 });
 
-		 var activeFields = _.filter(allFields, function(field) { // trim the fields to the ones I want to display
-		 	return _.indexOf($scope.companyInfo2, field.name) >= 0;
-		 });
+			 var activeFields = _.filter(allFields, function(field) { // trim the fields to the ones I want to display
+			 	return _.indexOf($scope.companyInfo2, field.name) >= 0;
+			 });
+			 deferred.resolve(activeFields);
+		});
+		return deferred.promise;
+	};
 
-		$scope.companyInfo = activeFields;
-		api.companies.getCompany($scope.companyId).then(function(data) {
+	var getCompanyInfo = function getCompanyInfo() {
+		var deferred = $q.defer();
+		api.companies.getCompany($scope.accountId).then(function(data) {
 			$scope.relatedTo.push({name: data.Name, id: data.Id});
-			var liveData = _.filter(data, function(value) {
+			// console.log('data', data);
+			var allData = _.map(data, function(field, key) {
+				return {name: key, value: field};
+			});
+			// console.log('all data', allData);
+			var liveData = _.filter(allData, function(field) {
 				return _.indexOf($scope.companyInfo2, field.name) >= 0;
-			});
-
-			var liveCompanyInfo = _.map(liveData, function(sfData, index) {
-				var index = _.indexOf(activeFields, value.name);
-				return activeFields[index].value = sfData;
 			})
-			_.forEach(data, function(salesforceValue, key) {
-				_.forEach($scope.companyInfo, function(value, index) {
-					if(value.name === key) {		
-						value.value = salesforceValue;
-					}
-				});
-			});
-			console.log("company info after company api:\n", $scope.companyInfo);
+			// console.log('company data', liveData);
+			deferred.resolve(liveData);
+			// console.log('company data', allData);
 		}); 
+		return deferred.promise;
+	};
 
-	});
+	var mergeCompanyData = function mergeCompanyData(fieldsPromise, companyInfoPromise) {
+	    	return $q.all([fieldsPromise, companyInfoPromise]).then(function(values) {
+	    		// console.log('values', values[0], values[1]);
+	    		var data = _.map(values[0], function(field, index) {
+	    			var val = _.find(values[1], function(value) {
+	    				return value.name === field.name;
+	    			});
+	    			// console.log('val', val);
+	    			field.value = val.value;
+	    			return field;
+	    		})
+	    		console.log('data', data);
+	    		$scope.companyInfo = data;
+	    	})
+	}
 
+	mergeCompanyData(getCompanyFields(), getCompanyInfo());
 		
 
-	$scope.contacts;
-	api.companies.getContacts($scope.companyId)
-		.then( function(contacts) {
-			$scope.contacts = contacts;
-		});
+	// $scope.contacts;
+	// api.companies.getContacts($scope.companyId)
+	// 	.then( function(contacts) {
+	// 		$scope.contacts = contacts;
+	// 	});
 
-	$scope.opportunities;
-	api.opportunities.getForCompany($scope.companyId)
-		.then( function(opportunities) {
-			$scope.opportunities = opportunities;
-			_.forEach(opportunities, function(value, index) {
-				$scope.relatedTo.push({name: value.Name, id: value.Id });	
-			});
+	// $scope.opportunities;
+	// api.opportunities.getForCompany($scope.companyId)
+	// 	.then( function(opportunities) {
+	// 		$scope.opportunities = opportunities;
+	// 		_.forEach(opportunities, function(value, index) {
+	// 			$scope.relatedTo.push({name: value.Name, id: value.Id });	
+	// 		});
 			
-		});
+	// 	});
 
 	
 	
