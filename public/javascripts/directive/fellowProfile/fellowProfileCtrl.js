f46335d3b4076eb8713070cbc76d0fdf0390c985
Jason Tarre <jason.tarre@gmail.com>
2016-02-20 13:50:02 -0500

    final page polish before pushing live.

--- public/javascripts/directive/fellowProfile/fellowProfileCtrl.js
+++ public/javascripts/directive/fellowProfile/fellowProfileCtrl.js
@@ -1,19 +1,31 @@
 angular.module('vfaDashboard').controller('fellowProfileCtrl', function($scope, $attrs, _, api) {
 
-$scope.fellowError = false;
-api.fellows.getFellow($scope.id).then(function(data) {
-	$scope.fellow = data;
-	console.log('fellow data: ', $scope.fellow);
-}, function(error) {
-	$scope.fellowError = "Sorry, please try again or contact JTD"
-}) // need to have id
-	
-$scope.updateInProgress = false;
-$scope.updateError = false;
-$scope.update = function update() {
-	$scope.updateInProgress = false;
-	$scope.updateError = false;
-	api.contacts.update() // this doesn't exist yet
-}
+    $scope.fellowError = false;
+    api.fellows.getFellow($scope.id).then(function(data) {
+            $scope.fellow = data;
+            console.log('fellow data: ', $scope.fellow);
+        }, function(error) {
+            $scope.fellowError = "Sorry, please try again or contact JTD"
+        }) // need to have id
+
+    $scope.$watch('id', function(newId) {
+        if (!newId) {
+            console.log('no new Id yet.');
+        } else {
+            api.fellows.getFellow($scope.id).then(function(data) {
+                    $scope.fellow = data;
+                    console.log('fellow data: ', $scope.fellow);
+                }, function(error) {
+                    $scope.fellowError = "Sorry, please try again or contact JTD"
+                }) // need to have id
+        }
+    })
+    $scope.updateInProgress = false;
+    $scope.updateError = false;
+    $scope.update = function update() {
+        $scope.updateInProgress = false;
+        $scope.updateError = false;
+        api.contacts.update() // this doesn't exist yet
+    }
 
 });
